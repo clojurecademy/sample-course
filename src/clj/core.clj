2bf79dfcf872d1740cfde3c85f4cbe5ef5b6e62c
ertugrulcetin <ertu413@gmail.com>
2017-09-06 09:25:35 +0200

    more doc code

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -92,7 +92,7 @@
                          (text
                            (p "To understand Clojure comprehensively we are going to write some basic math functions in this section.")))
                        (instruction 'ins-subj-math-fns
-                                    (run-pre-tests? false)
+                                    (run-pre-tests? true)
                                     (initial-code :none)
                                     (rule :no-rule? true)
 
@@ -125,7 +125,169 @@
                                                        (is (= (my-multi 1) 1))
                                                        (is (= (my-multi 2 1) 2))
                                                        (is (= (my-multi 1 2 3 4 5 6) 720)))))
-                       'subj-math-fns)))))
+                       'subj-math-fns)
+
+                     (subject
+                       'subj-initial-code-ex-1
+                       "Let's see some initial code"
+
+                       (learn
+                         (text
+                           (p "Check code in the editor")))
+                       (instruction 'ins-initial-code-ex-1
+                                    (run-pre-tests? false)
+                                    (initial-code (println "Hello, World"))
+                                    (rule :no-rule? true)
+
+                                    (sub-instruction 'sub-initial-code-ex-1
+                                                     (text
+                                                       (p "Please click the Run button"))
+                                                     (testing
+                                                       ;;mock true for demo
+                                                       (is (true? true)))))
+                       'initial-code-ex-1)
+
+                     (subject
+                       'subj-initial-code-ex-2
+                       "Let's see some initial code 2"
+
+                       (learn
+                         (text
+                           (p "Check code in the editor")))
+                       (instruction 'ins-initial-code-ex-2
+                                    (run-pre-tests? false)
+                                    (initial-code "\n\n(defn your-fn\n  [x y]\n  )")
+                                    (rule :no-rule? true)
+
+                                    (sub-instruction 'sub-initial-code-ex-2
+                                                     (text
+                                                       (p "Please click the Run button"))
+                                                     (testing
+                                                       ;;mock true for demo
+                                                       (is (true? true)))))
+                       'initial-code-ex-2)
+
+                     (subject
+                       'subj-initial-code-ex-3
+                       "Let's see some initial code 3"
+
+                       (learn
+                         (text
+                           (p "Check code in the editor")))
+                       (instruction 'ins-initial-code-ex-3
+                                    (run-pre-tests? false)
+                                    (initial-code (str "\n\n(println \"Scalars: \\n\")\n\n\n"
+                                                       "(println \"Type of 1 is: \" (type 1) \"\\n\")\n\n\n"
+                                                       "(println \"Type of 1.2 is: \" (type 1.2) \"\\n\")\n\n\n"
+                                                       "(println \"Type of 1N is: \" (type 1N) \"\\n\")\n\n\n"
+                                                       "(println \"Type of 1.5M is: \" (type 1.5M) \"\\n\")\n\n\n"
+                                                       "(println \"Type of 1.2e4 is: \" (type 1.2e4) \"\\n\")\n\n\n"
+                                                       "(println \"Type of 1/3 is: \" (type 1/3) \"\\n\")\n\n\n"
+                                                       "(println \"Type of \\\"HEY!\\\" is: \" (type \"HEY!\") \"\\n\")\n\n\n"
+                                                       "(println \"Type of \\\\a is: \" (type \\a) \"\\n\")\n\n\n"
+                                                       "(println \"Type of :my-keyword is: \" (type :my-keyword) \"\\n\")\n\n\n"
+                                                       "(println \"Type of 'my-s is: \" (type 'my-s) \"\\n\")"))
+                                    (rule :no-rule? true)
+
+                                    (sub-instruction 'sub-initial-code-ex-3
+                                                     (text
+                                                       (p "Please click the Run button"))
+                                                     (testing
+                                                       ;;mock true for demo
+                                                       (is (true? true)))))
+                       'initial-code-ex-3)
+
+                     (subject
+                       'subj-initial-code-ns-ex
+                       "Let's see some initial code"
+
+                       (learn
+                         (text
+                           (p "Check code in the editor")))
+                       (instruction 'ins
+                                    (run-pre-tests? false)
+                                    (initial-code "\n(ns clj.core\n  (:require [clojure.string :as str]))\n\n(defn your-fn\n  []\n  )")
+                                    (rule :no-rule? true)
+
+                                    (sub-instruction 'sub-ins
+                                                     (text
+                                                       (p "Please click the Run button"))
+                                                     (testing
+                                                       ;;mock true for demo
+                                                       (is (true? true))))))
+
+                     (subject 'subj-rule-restricted-ex
+                              "Last Element"
+
+                              (learn
+                                (text
+                                  (p "Write a function called " (hi "my-last") " which returns the last element in a sequence.")))
+
+                              (instruction 'ins-rule-restricted-ex
+                                           (run-pre-tests? false)
+                                           (initial-code :none)
+                                           (rule :restricted-fns '[last])
+
+                                           (sub-instruction 'sub-ins-rule-restricted-ex
+                                                            (text
+                                                              (p "Tests need to pass:")
+                                                              (code (= (my-last [1 2 3 4 5]) 5))
+                                                              (code "(= (my-last '(5 4 3)) 3)")
+                                                              (code (= (my-last ["b" "c" "d"]) "d")))
+                                                            (testing
+                                                              (is (= (my-last [1 2 3 4 5]) 5) nil :advanced)
+                                                              (is (= (my-last '(5 4 3)) 3) nil :advanced)
+                                                              (is (= (my-last ["b" "c" "d"]) "d") nil :advanced))))
+                              'rule-restricted-ex)
+
+                     (subject 'subj-rule-only-one-fn-ex
+                              "Last Element"
+
+                              (learn
+                                (text
+                                  (p "Write a function called " (hi "my-last") " which returns the last element in a sequence.")))
+
+                              (instruction 'ins-rule-only-one-fn-ex
+                                           (run-pre-tests? false)
+                                           (initial-code :none)
+                                           (rule :only-use-one-fn? true)
+
+                                           (sub-instruction 'sub-ins-rule-only-one-fn-ex
+                                                            (text
+                                                              (p "Tests need to pass:")
+                                                              (code (= (my-last [1 2 3 4 5]) 5))
+                                                              (code "(= (my-last '(5 4 3)) 3)")
+                                                              (code (= (my-last ["b" "c" "d"]) "d")))
+                                                            (testing
+                                                              (is (= (my-last [1 2 3 4 5]) 5) nil :advanced)
+                                                              (is (= (my-last '(5 4 3)) 3) nil :advanced)
+                                                              (is (= (my-last ["b" "c" "d"]) "d") nil :advanced))))
+                              'rule-only-one-fn-ex)
+
+                     (subject 'subj-rule-required-fn-ex
+                              "Last Element"
+
+                              (learn
+                                (text
+                                  (p "Write a function called " (hi "my-last") " which returns the last element in a sequence.")))
+
+                              (instruction 'ins-rule-required-fn-ex
+                                           (run-pre-tests? false)
+                                           (initial-code :none)
+                                           (rule :required-fns '[last]
+                                                 :only-use-one-fn? true)
+
+                                           (sub-instruction 'sub-ins-rule-required-fn-ex
+                                                            (text
+                                                              (p "Tests need to pass:")
+                                                              (code (= (my-last [1 2 3 4 5]) 5))
+                                                              (code "(= (my-last '(5 4 3)) 3)")
+                                                              (code (= (my-last ["b" "c" "d"]) "d")))
+                                                            (testing
+                                                              (is (= (my-last [1 2 3 4 5]) 5) nil :advanced)
+                                                              (is (= (my-last '(5 4 3)) 3) nil :advanced)
+                                                              (is (= (my-last ["b" "c" "d"]) "d") nil :advanced))))
+                              'rule-required-fn-ex)))))
 
 
 (defcoursetest my-test-1
@@ -160,7 +322,6 @@
                  [& args]
                  (apply - args)))
 
-
 (defcoursetest my-test-6
                [ch-intro sub-ch-basics subj-more-math-fns-with-pre-tests-on ins-subj-math-fns sub-ins-my-multi]
                (defn my-add
@@ -174,3 +335,49 @@
                (defn my-multi
                  [& args]
                  (apply * args)))
+
+(defcoursetest my-test-7
+               [ch-intro sub-ch-basics subj-initial-code-ex-1 ins-initial-code-ex-1 sub-initial-code-ex-1]
+               ;;empty vec is for mocking(defcoursetest body can't be empty), it has no effect
+               [])
+
+(defcoursetest my-test-8
+               [ch-intro sub-ch-basics subj-initial-code-ex-2 ins-initial-code-ex-2 sub-initial-code-ex-2]
+               [])
+
+(defcoursetest my-test-9
+               [ch-intro sub-ch-basics subj-initial-code-ex-3 ins-initial-code-ex-3 sub-initial-code-ex-3]
+               [])
+
+(defcoursetest my-test-10
+               [ch-intro sub-ch-basics subj-initial-code-ns-ex ins sub-ins]
+               [])
+
+(defcoursetest my-test-11
+               [ch-intro sub-ch-basics subj-rule-restricted-ex ins-rule-restricted-ex sub-ins-rule-restricted-ex]
+               (defn my-last
+                 [x]
+                 ;if you comment out last fn you will get an exception for :restricted-fns
+                 ;(last x)
+                 (nth x (- (count x) 1))))
+
+(defcoursetest my-test-12
+               [ch-intro sub-ch-basics subj-rule-only-one-fn-ex ins-rule-only-one-fn-ex sub-ins-rule-only-one-fn-ex]
+               ;if you comment out my-fn you will get an exception for :only-use-one-fn?
+               ;(defn my-fn
+               ;  [])
+               (defn my-last
+                 [x]
+                 (nth x (- (count x) 1))))
+
+(defcoursetest my-test-13
+               [ch-intro sub-ch-basics subj-rule-required-fn-ex ins-rule-required-fn-ex sub-ins-rule-required-fn-ex]
+               ;if you comment out my-fn you will get an exception for :required-fns and :only-use-one-fn?
+               ;(defn my-fn
+               ;  [])
+               (defn my-last
+                 [x]
+                 ;;if you don't use last you will get an exception
+                 (last x)))
+
+
