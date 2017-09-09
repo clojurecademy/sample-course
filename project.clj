77e163ae119ba53bb36a35a0f1146f2c6ab3ec96
ertugrulcetin <ertu413@gmail.com>
2017-09-09 17:47:29 +0200

    formatting

--- project.clj
+++ project.clj
@@ -1,11 +1,18 @@
 (defproject sample-project "0.1.0-SNAPSHOT"
+
   :description "FIXME: write description"
+
   :url "http://example.com/FIXME"
+
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojurecademy/dsl "0.3.5"]]
+
   :eval-in :leiningen
+
   :plugins [[clojurecademy/lein-clojurecademy "0.1.2"]]
+
   :clojurecademy {:course-map clj.core/course-map
                   :helper-fns-ns clj.helper-fns})
