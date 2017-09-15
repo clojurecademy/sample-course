b65696319bce7524b729885589475f182af7cff3
ertugrulcetin <ertu413@gmail.com>
2017-09-15 17:20:51 +0200

    update lib version 1.5 final

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
 
   :eval-in :leiningen
 
-  :plugins [[lein-clojurecademy "0.1.1"]]
+  :plugins [[lein-clojurecademy "0.1.5"]]
 
   :clojurecademy {:course-map clj.core/course-map
                   :helper-fns-ns clj.helper-fns})
